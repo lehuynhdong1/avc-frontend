6331d6767d52f83fd5ccdc7053c696835e6cb014
Ngo Van Sang <sangnvse130361@fpt.edu.vn>
2021-06-03 11:06:24 +0000

    Login sangnv

--- libs/shared/auth/login/ui/src/lib/login.component.ts
+++ libs/shared/auth/login/ui/src/lib/login.component.ts
@@ -1,9 +1,76 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Router } from  '@angular/router';
+import {
+  TUI_INPUT_PASSWORD_DEFAULT_OPTIONS,
+  TUI_INPUT_PASSWORD_OPTIONS,
+  TUI_VALIDATION_ERRORS
+} from '@taiga-ui/kit';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+
+//TODO: config validator
+// const requireEmailValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+//     const email = control.get('email');
+//     return email != null && email.value.lenght != 0 ? {requireEmail: true} : null;
+//   };
+
 
 @Component({
   selector: 'adc-frontend-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: TUI_INPUT_PASSWORD_OPTIONS,
+      useValue: {
+        ...TUI_INPUT_PASSWORD_DEFAULT_OPTIONS,
+        icons: {
+          hide: 'tuiIconEyeClosed',
+          show: 'tuiIconEyeOpen'
+        }
+      }
+    },
+    {
+      provide: TUI_VALIDATION_ERRORS,
+      useValue: {
+        required: 'Input value is required!',
+        email: 'Please enter a valid email address',
+      }
+    }
+  ]
 })
-export class SharedLoginComponent {}
+
+export class SharedLoginComponent implements OnInit {
+
+  constructor(private formBuilder: FormBuilder,private router: Router) { };
+  isSubmitted = false;
+  loginForm!: FormGroup;
+
+
+  // readonly loginForm = new FormGroup({
+  //   email: new FormControl('', [Validators.required,Validators.email]),
+  //   password: new FormControl('', Validators.required),
+  //   remember: new FormControl(false)
+  // });
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', [Validators.required,Validators.email]),
+      password: new FormControl('', Validators.required),
+      remember: new FormControl(false)
+    });
+  }
+  //TODO: Validator chay bằng Angular
+  // get formControls() { return this.loginForm.controls; }
+  
+  login(){
+    console.log(this.loginForm.value);
+    this.isSubmitted = true;
+    if(this.loginForm.invalid){
+      return;
+    }
+    //TODO: Add Service
+    // this.authService.login(this.loginForm.value);
+    this.router.navigateByUrl('/');
+  }
+}
