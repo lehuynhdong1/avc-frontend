cd6d58cc1f4290c2ffa0a945f5ed7b3f2490253d
thanhhoa214 <hoa.nguyen@nng.bz>
2021-07-31 20:44:03 +0700

    Disable logger

--- libs/shared/state-management/src/lib/state-management.module.ts
+++ libs/shared/state-management/src/lib/state-management.module.ts
@@ -1,5 +1,5 @@
-import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
-import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+// import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+// import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
 import { NgxsModule } from '@ngxs/store';
 import { AppConfig } from '@shared/app-config';
@@ -23,9 +23,9 @@ export function StateManagementModulesWithConfig(config: AppConfig) {
   return [
     NgxsModule.forRoot([], { developmentMode: !config.production }),
     NgxsStoragePluginModule.forRoot({ key: [LoginState] }),
-    NgxsLoggerPluginModule.forRoot({ disabled: config.production }),
-    NgxsReduxDevtoolsPluginModule.forRoot({ disabled: config.production }),
     NgxsResetPluginModule.forRoot(),
+    // NgxsLoggerPluginModule.forRoot({ disabled: config.production }),
+    // NgxsReduxDevtoolsPluginModule.forRoot({ disabled: config.production }),
     ...featureStates
   ];
 }
